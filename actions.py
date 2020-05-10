1780e47a3a377bb530841308cf57ff368167fb1a
Ayodeji <ayodejiibimilua4@gmail.com>
2020-05-10 08:10:01 +0100

    actions file edited

--- actions.py
+++ actions.py
@@ -1,6 +1,6 @@
 # This files contains your custom actions which can be used to run
 # custom Python code.
-#
+
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/core/actions/#custom-actions/
 
@@ -11,13 +11,13 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.e
+from rasa_sdk.
 
 from newsapi import NewsApiClient
 
 newsapi = NewsApiClient(api_key="aac1749d71c1403e9c3be7b474fc8e0f")
 
-top_headlines = newsapi.get_top_headlines()
+
 
 country_list = [ae, ar, at, au, be, bg, br, ca, ch, cn, co, cu, cz, de, eg, fr, gb, gr, hk, gu
 id, ie, il, in, it, jp, kr, lt, lv, ma, mx, my, ng, nl, no, nz, ph, pl, pt, ro, rs, ru, sa, se,
@@ -30,26 +30,23 @@ class ActionGetNews(Action):
 
     def run(self, dispatcher, tracker, domain):
 
-        dispatcher.utter_message(text="Hello World!")
+        q = tracker.get_slot("query")
+        source = tracker.get_slot("source")
+        category = tracker.get_slot("category")
+        country = tracker.get_slot("country")
+      
+        values_dict = {"q":q, "source": source, "category": category,
+        "country": country, "pageSize": 5}
+
+        if source and (country or category):
+            dispatcher.utter_message(text="Here is what I found")
+        else:
+            top_headlines = newsapi.get_top_headlines(**values_dict)
+            dispatcher.utter_message(text="Here is what I found")
+
+        
+
+        
 
         return []
 
-
-country
-query
-source
-category
-country, query
-country, source
-country, category
-query, source
-query, category
-source, category
-country, query, source
-country, query, category
-country, source,category
-query, source, category
-
-def performsearch():
-    return("print hey")
-    
